ddd3af0559bb8bd1c38fa275d591f729ae32bbe8
Adam Levy <Alevy01@gmail.com>
2017-02-21 16:07:56 -0500

    misc update

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,14 +13,13 @@ module.exports = {
       password : req.body.password
     }
     bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, function(err, hash) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
         var q = "INSERT INTO users (f_name, l_name, username, hash, salt) VALUES ('"+user.f_name +"'," + "'"+user.l_name +"'," + "'"+user.username +"'," + "'"+hash +"'," + "'"+salt +"');";
-        console.log(q);
         var promise = db.executeQuery(q);
         promise.then(function(rows){
           
           req.session.regenerate(function(){
-            req.session.user = user
+            req.session.user = user;
             res.status(200).end();
           });
           
