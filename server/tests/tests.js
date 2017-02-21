3d7c80e187f102749b528de7f8c36266130434d0
smistry1121 <sagar.mistry@knights.ucf.edu>
2017-02-21 15:01:22 -0500

    added test cases to 'Words' for TDD

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -29,7 +29,27 @@ describe('Users', function() {
 });
 
 describe('Words', function(){
-
+    it('Should get "x amount" the words from /words', function(done){
+        chai.request(server)
+            .get('/words')
+            .end(function(err,res){
+                res.should.have.status(200);
+                res.should.be.json;
+                res.to.have.lengthOf(50);
+            })
+    });
+    it('Should add a nmenonic to a word', function(done){
+        chai.request(server)
+            .post('/words')
+            .end(function(err,res){
+                res.should.have.status(200);
+            });
+    });
+    it('Should rate a mnemonic' function(done){
+        chai.request(server)
+            .post('/rate')
+            .res.should.have.status(200);
+            //test rating of word
 });
 
 describe('Votes', function(){
