4c8632075a310b960be7e83b662f3533bf55e056
Adam Levy <Alevy01@gmail.com>
2017-02-22 13:08:59 -0500

    finally a correct create script

--- server/database/create.sql
+++ server/database/create.sql
@@ -1,7 +1,5 @@
-# CREATE SCRIPT
-
-CREATE TABLE mnemonics (mn_id SERIAL PRIMARY KEY, word_id FOREIGN KEY(word_id), user_id FOREIGN KEY(user_id), mnemonic VARCHAR(200) NOT NULL, lat DECIMAL, long DECIMAL, rating INTEGER);
-CREATE TABLE words (word_id SERIAL PRIMARY KEY, word VARCHAR(200) NOT NULL, def VARCHAR(200) NOT NULL)
+CREATE TABLE words (word_id SERIAL PRIMARY KEY, word VARCHAR(200) NOT NULL, def VARCHAR(200) NOT NULL);
 CREATE TABLE users (user_id SERIAL PRIMARY KEY, f_name VARCHAR(20) NOT NULL, l_name VARCHAR(20) NOT NULL, username VARCHAR(20) NOT NULL UNIQUE, hash VARCHAR(60) NOT NULL, salt VARCHAR(50) NOT NULL);
+CREATE TABLE mnemonics (mn_id SERIAL PRIMARY KEY, word_id INTEGER references words(word_id), user_id INTEGER references users(user_id), mnemonic VARCHAR(200) NOT NULL, lat DECIMAL, long DECIMAL, rating INTEGER);
 CREATE TABLE session ("sid" varchar NOT NULL COLLATE "default", "sess" json NOT NULL, "expire" timestamp(6) NOT NULL) WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
