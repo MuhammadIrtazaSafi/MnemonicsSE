58ee8d7ce0d1f84c1c38af60b56cab821c6616e2
Adam Levy <Alevy01@gmail.com>
2017-03-24 15:03:17 -0400

    mnemonic voting

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -1,5 +1,17 @@
+var db = require('../database/db.js');
+
 module.exports = {
   test : function(req, res){
     res.send("server is up.");
+  },
+  addVote : function(req, res){
+    var q = "UPDATE mnemonics SET rating=rating+"+req.body.vote+" WHERE mn_id="+req.body.mn_id+";";
+    var promise = db.executeQuery(q);
+    promise.then(function(rows){
+      res.status(200).end();
+    })
+    .catch(function(err){
+      res.stats(400).end();
+    })
   }
 }
