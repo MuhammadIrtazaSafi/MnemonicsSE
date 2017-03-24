49292b6a49610bac36bfc32a99b3e724c32bbd7c
smistry1121 <sagar.mistry@knights.ucf.edu>
2017-03-24 14:34:51 -0400

    merging

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var sessionStore = require('connect-pg-simple');
 var app = express();
 var db = require('./database/db.js');
 app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -14,9 +12,9 @@ var conString = 'postgres://postgres@localhost/mnemonics';
 app.use(session({
   store : new (sessionStore(session))({conString}),
   pg : db.pg,
-  secret: "SO SECRET.",
-  resave: true,
-  saveUninitialized : false,
+  secret: "this is our cookie secret.",
+  resave: false,
+  saveUninitialized : true,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 10000 } 
 }));
 module.exports = app;
@@ -25,19 +23,12 @@ var user_routes = require('./routes/users.js');
 var vote_routes = require('./routes/votes.js');
 var word_routes = require('./routes/words.js');
 
-
-function restrict(req, res, next) {
-  console.log(req.cookies);
-  if (req.session.user && req.cookies["connect.sid"] == req.body.sid) {
-    next();
-  } 
-}
-
-
-app.get('/test', restrict, user_routes.test);
+//Example endpoint. 
+app.get('/test', user_routes.test);
 app.post('/registerUser', user_routes.registerUser);
 app.post('/login', user_routes.login);
-app.post('/logout', user_routes.logout);
+app.get('/words', word_routes.words);
+app.post('/mnemonic', word_routes.addMnemonic);
 
 var port = 8000;
 app.listen(port);
