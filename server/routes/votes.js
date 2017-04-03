4d88f645b91603dede04353024d7734334a5ae5f
smistry1121 <sagar.mistry@knights.ucf.edu>
2017-04-03 10:10:25 -0400

    Added routes for 'getMnemonic', also implemented alpha voting system with upvote and down vote

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -5,7 +5,18 @@ module.exports = {
     res.send("server is up.");
   },
   addVote : function(req, res){
-    var q = "UPDATE mnemonics SET rating=rating+"+req.body.vote+" WHERE mn_id="+req.body.mn_id+";";
+    var q = "UPDATE mnemonics SET rating=rating+1 WHERE mn_id="+req.body.mn_id+";";
+    console.log(q);
+    var promise = db.executeQuery(q);
+    promise.then(function(rows){
+      res.status(200).end();
+    })
+    .catch(function(err){
+      res.stats(400).end();
+    })
+  },
+  subtractVote : function(req, res){
+    var q = "UPDATE mnemonics SET rating=rating-1 WHERE mn_id="+req.body.mn_id+";";
     var promise = db.executeQuery(q);
     promise.then(function(rows){
       res.status(200).end();
--- server/routes/words.js
+++ server/routes/words.js
@@ -11,7 +11,6 @@ module.exports = {
   addMnemonic : function(req,res){
     q = "(SELECT user_id FROM users WHERE username ='"+req.session.user.username+"')";
     x = "INSERT INTO mnemonics (user_id, word_id, mnemonic, lat, long, rating) VALUES ("+q+",'"+req.body.word_id+"','"+ req.body.mnemonic+"','"+ req.body.lat+"','"+ req.body.long+"',0)";
-    console.log(x); 
     var promise = db.executeQuery(x);
     promise.then(function(result){  
       res.status(200).end();
@@ -19,6 +18,19 @@ module.exports = {
     .catch(function(err){
       res.status(400).end();
     })
-  }
+  },
+  getMnemonic : function(req,res){
+    console.log('test');
+    q = "(SELECT user_id FROM users WHERE username ='"+req.session.user.username+"')";
+    x = "SELECT * FROM mnemonics WHERE user_id = "+q+" AND word_id = '"+req.query.word_id+"'";
+    console.log(x);
+    var promise = db.executeQuery(x);
+    promise.then(function(result){  
+      res.json(result.rows).end();
+    })
+    .catch(function(err){
+      res.status(400).end();
+    })
+   }
 
 }
