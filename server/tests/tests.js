f1cb0ac0c34153344cd56187e32f922c8280623d
beastafoe <austink2191@gmail.com>
2017-04-13 21:18:41 -0400

    All test incorporated
    Awaiting testing via Mocha/Chai

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -15,43 +15,109 @@ describe('Users', function() {
         done();
       });
   });
-  
+
   it('Should return 200 from /login POST', function(done) {
+      chai.request(server)
+          .post('/login')
+          .send({'username' : 'test', 'password' : 'password'})
+          .end(function(err, res){
+              res.should.have.status(200);
+              done();
+          });
+  });
+
+  /*it('Should return 200 from /delete POST', function(done) {
     chai.request(server)
-      .post('/login')
-      .send({'username' : 'test', 'password' : 'password'})
+      .post('/delete')
       .end(function(err, res){
         res.should.have.status(200);
         done();
       });
-  });
+  });*/
 
+    it('Should return 200 from /logout POST', function(done) {
+        chai.request(server)
+            .post('/logout')
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
 });
 
 describe('Words', function(){
-    it('Should get "x amount" the words from /words', function(done){
+
+    it('Should return 200 from /login POST', function(done) {
+        chai.request(server)
+            .post('/login')
+            .send({'username' : 'test', 'password' : 'password'})
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+    it('Should get all the words from /words', function(done){
         chai.request(server)
             .get('/words')
             .end(function(err,res){
                 res.should.have.status(200);
                 res.should.be.json;
                 res.to.have.lengthOf(50);
+                done();
             })
     });
-    it('Should add a nmenonic to a word', function(done){
+
+    it('Should add a mnenonic to a word', function(done){
         chai.request(server)
-            .post('/words')
+            .post('/addMnemonic')
+            .send({'username' : 'test', 'word' : 'PLACEHOLDER', 'mnenomic' : 'word or phrase to remember word', 'lat' : 0.0, 'long' : 0.0})
             .end(function(err,res){
                 res.should.have.status(200);
+                done();
             });
     });
-    it('Should rate a mnemonic' function(done){
+
+    it('Should return all mnemonics created by the user', function(done){
         chai.request(server)
-            .post('/rate')
-            .res.should.have.status(200);
-            //test rating of word
-});
+            .post('/getUserMnemonic')
+            .end(function(err,res){
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+    it('Should return Mnemonics based on location', function(done){
+        chai.request(server)
+            .get('/locationMnemonic')
+            .end(function(err,res){
+                res.should.have.status(200);
+                res.should.be.json;
+                done();
+            });
+    });
+
 
 describe('Votes', function(){
+
+    it('Should add upvote to given mnemonic', function(done) {
+        chai.request(server)
+            .post('/addVote')
+            .send({'mn_id' : 'PLACEHOLDER'})
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+    it('Should add downvote to given mnemonic', function(done) {
+        chai.request(server)
+            .post('/subtractVote')
+            .send({'mn_id' : 'PLACEHOLDER'})
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
 
 });
